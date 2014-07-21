e97a94606d1cb7e519a8dbf158a0d0f6ab45d266
Francis Deslauriers <francis.deslauriers@efficios.com>
2014-07-21 10:54:43 -0400

    Parse mutatee and create tp in mutator for testing purposes

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-CFLAGS= -g -O0 
+CFLAGS= -g -O0 -Wall 
 
 all: mutator mutatee
 
 mutator: mutator.cpp 
-	g++ $(CFLAGS)  $^ -llttng-ust -llttng-ust-tracepoint -o $@
+	g++ $(CFLAGS)  $^ -llttng-ust -llttng-ust-tracepoint -ldyninstAPI -o $@
 mutatee: mutatee.c
 	gcc $(CFLAGS) $^ -o $@
 clean:
@@ -12,7 +12,7 @@ run: mutator
 	lttng create
 	lttng enable-event -a -u
 	lttng start
-	env LD_PRELOAD=/usr/local/lib/liblttng-ust.so ./mutator
+	env DYNINSTAPI_RT_LIB=/usr/lib/libdyninstAPI_RT.so LD_PRELOAD=/usr/local/lib/liblttng-ust.so ./mutator
 	lttng stop
 	lttng view
 	lttng destroy
--- mutator.cpp
+++ mutator.cpp
@@ -8,70 +8,73 @@
 #include <lttng/tracepoint.h>
 #include <lttng/ringbuffer-config.h>
 
-#define NB_CHAMP 3
+#include <BPatch.h>
+#include <BPatch_function.h>
 extern "C" int tracepoint_register_lib(struct tracepoint * const *tracepoints_start, int nb);
 
 extern "C" {
-void add_int_event_field( struct lttng_event_field *field, char *name)
-{
-	struct lttng_event_field f = {
-		.name = name,
-		.type = __type_integer(int, LITTLE_ENDIAN, 10, none),
-		.nowrite = 0
-	};
+	void add_int_event_field( struct lttng_event_field *field, char *name)
+	{
+		struct lttng_event_field f = {
+			.name = name,
+			.type = __type_integer(int, LITTLE_ENDIAN, 10, none),
+			.nowrite = 0
+		};
 
-	memcpy(field, &f, sizeof(struct lttng_event_field));
-}
+		memcpy(field, &f, sizeof(struct lttng_event_field));
+	}
 
-void add_float_event_field( struct lttng_event_field *field, char *name)
-{
-	struct lttng_event_field f = {
-		.name = name,
-		.type = __type_float(float),
-		.nowrite = 0
-	};
+	void add_float_event_field( struct lttng_event_field *field, char *name)
+	{
+		struct lttng_event_field f = {
+			.name = name,
+			.type = __type_float(float),
+			.nowrite = 0
+		};
 
-	memcpy(field, &f, sizeof(struct lttng_event_field));
-}
-void add_char_event_field( struct lttng_event_field *field, char *name)
-{
-	struct lttng_event_field f = {
-		.name = name,
-		.type = __type_integer(char, LITTLE_ENDIAN, 10, none),
-		.nowrite = 0
-	};
+		memcpy(field, &f, sizeof(struct lttng_event_field));
+	}
+	void add_char_event_field( struct lttng_event_field *field, char *name)
+	{
+		struct lttng_event_field f = {
+			.name = name,
+			.type = __type_integer(char, LITTLE_ENDIAN, 10, none),
+			.nowrite = 0
+		};
 
-	memcpy(field, &f, sizeof(struct lttng_event_field));
-}
+		memcpy(field, &f, sizeof(struct lttng_event_field));
+	}
 }
 void fake_probe(struct lttng_event* __tp_data)
 {}
+using namespace std;
 int main (int argc, const char* argv[]) 
 {
+	BPatch bpatch;
+	//BPatch_addressSpace *handle = bpatch.processCreate("./mutatee", NULL);
+	BPatch_addressSpace *handle = bpatch.openBinary("./mutatee", NULL);
+	BPatch_image *image = handle->getImage();
+	vector<BPatch_function*> functions;
+	image->findFunction("getAnswer", functions);
+
+	vector<BPatch_localVar *> *params = functions[0]->getParams();
+	int nb_field = params->size();
+
 	char *name = (char*)malloc(sizeof(char) *50);
 	char *sign = (char*)malloc(sizeof(char) *50);
-	sprintf(name,"test:testevent");
-	sprintf(sign,"testevent");
+	sprintf(name,"prvdr:event");
+	sprintf(sign,"event");
 
 	int i, j;
-	int champs0 = 18;
-	float champs1 = 42.212;
-	char champs2 = 'f';
 
-	char *nom_champs[NB_CHAMP];
-	nom_champs[0] =(char*) "int1";
-	nom_champs[1] =(char*) "float2";
-	nom_champs[2] =(char*) "char3";
-
-	int type[3];
-	type[0] = 0;
-	type[1] = 1;
-	type[2] = 2;
-
-	size_t size_champs[NB_CHAMP];
-	size_champs[0] = sizeof(int);
-	size_champs[1] = sizeof(float);
-	size_champs[2] = sizeof(char);
+	char **fieldNames = (char**) malloc(sizeof(char *) *nb_field );
+	size_t *size_champs = (size_t *) malloc(sizeof(size_t)*nb_field);
+	for(int i = 0;i < nb_field ; ++i)
+	{
+		fieldNames[i] = (char*) malloc(sizeof(char) *50);
+		strncpy(fieldNames[i], (*params)[i]->getName(), 50);
+		size_champs[i] = (*params)[i]->getType()->getSize();
+	}
 
 	//Tracepoint
 	struct tracepoint t= {
@@ -87,13 +90,44 @@ int main (int argc, const char* argv[])
 	//const struct tracepoint *test_tracepoints[] = {&t};
 	struct tracepoint **test_tracepoint = (struct tracepoint**) malloc(sizeof(struct tracepoint*));
 	test_tracepoint[0] = &t;
-	tracepoint_register_lib(test_tracepoint, NB_CHAMP);
+	tracepoint_register_lib(test_tracepoint, nb_field);
 
 	//Event fields
-	struct lttng_event_field *event_fields = (struct lttng_event_field* ) malloc(sizeof(struct lttng_event_field)*NB_CHAMP);
-	add_int_event_field(&event_fields[0], nom_champs[0]);
-	add_float_event_field(&event_fields[1], nom_champs[1]);
-	add_char_event_field(&event_fields[2], nom_champs[2]);
+	struct lttng_event_field *event_fields = (struct lttng_event_field* ) malloc(sizeof(struct lttng_event_field)*nb_field);
+
+	size_t __event_len = 0;
+	for(int i = 0;i < nb_field ; ++i)
+	{
+		// Add a field depending on the type of the parameter
+		switch((*params)[i]->getType()->getDataClass())
+		{
+			case BPatch_dataScalar:
+			{
+				string typeName = (*params)[i]->getType()->getName();
+				if(typeName == "char")
+				{
+					add_char_event_field(&event_fields[i], fieldNames[i]);
+					__event_len += lib_ring_buffer_align(__event_len, lttng_alignof(char));
+					__event_len += sizeof(char);
+				}
+				else if (typeName == "short int")
+				{
+				}
+				else
+				{
+					add_int_event_field(&event_fields[i], fieldNames[i]);
+					__event_len += lib_ring_buffer_align(__event_len, lttng_alignof(int));
+					__event_len += sizeof(int);
+				}
+				break;
+			}
+			default:
+			{
+				cout<<"Dataclass unsupported"<<endl;
+				break;
+			}
+		}
+	}
 
 	//Event description
 	struct lttng_event_desc event_desc_1 = {
@@ -101,7 +135,7 @@ int main (int argc, const char* argv[])
 		.probe_callback = (void (*)()) fake_probe ,
 		.ctx = NULL,
 		.fields = (const struct lttng_event_field *) event_fields,
-		.nr_fields = NB_CHAMP,
+		.nr_fields =(unsigned int) nb_field,
 		.loglevel = NULL,
 		.signature = sign,
 	};
@@ -113,7 +147,7 @@ int main (int argc, const char* argv[])
 	event_desc[0] = event_d;
 	//probe description
 	struct lttng_probe_desc desc = {
-		.provider = "test",
+		.provider = "prvdr",
 		.event_desc = (const struct lttng_event_desc **)event_desc,
 		.nr_events = 1,
 		.head = { NULL, NULL },
@@ -129,61 +163,33 @@ int main (int argc, const char* argv[])
 
 	for (i = 0; i < 10; ++i)
 	{
-		struct tracepoint_probe *__tp_probe;
-
-		__tp_probe = test_tracepoint[0]->probes;
-		void *__tp_data =__tp_probe->data;
-
+		void *__tp_data = test_tracepoint[0]->probes->data;
 		struct lttng_event *__event = (struct lttng_event *) __tp_data;
 		struct lttng_channel *__chan = __event->chan;
 		struct lttng_ust_lib_ring_buffer_ctx __ctx;
-		size_t __event_len, __event_align;
+		size_t __event_align;
 		int __ret;
 
-    		__event_len = 0;
-		for(j = 0; j < NB_CHAMP; ++j)
-		{
-			switch(type[j]){
-				case 0:
-					__event_len += lib_ring_buffer_align(__event_len, lttng_alignof(int));
-					__event_len += sizeof(int);
-					break;
-				case 1:
-					__event_len += lib_ring_buffer_align(__event_len, lttng_alignof(float));
-					__event_len += sizeof(float);
-					break;
-				case 2:
-					__event_len += lib_ring_buffer_align(__event_len, lttng_alignof(char));
-					__event_len += sizeof(char);
-					break;
-				default:
-					exit(-1);
-			}
-		}
 		__event_align = 0;
+		//Must be done in the mutatee address space
 		lib_ring_buffer_ctx_init(&__ctx, __chan->chan, __event, __event_len,
 				__event_align, -1, __chan->handle);
 		__ctx.ip = __builtin_return_address(0);
 		__ret = __chan->ops->event_reserve(&__ctx, __event->id);
 		if (__ret < 0)
 			return 0;
-		for(j = 0; j < NB_CHAMP; ++j)
+		for(j = 0; j < nb_field; ++j)
 		{
-			switch(type[j]){
-				case 0:
-					__chan->ops->event_write(&__ctx, &(champs0), sizeof(int));
-					lib_ring_buffer_align(__event_len, lttng_alignof(int));
-					break;
-				case 1:
-					__chan->ops->event_write(&__ctx, &(champs1), sizeof(float));
-					lib_ring_buffer_align(__event_len, lttng_alignof(float));
-					break;
-				case 2:
-					__chan->ops->event_write(&__ctx, &(champs2), sizeof(char));
-					lib_ring_buffer_align(__event_len, lttng_alignof(char));
-					break;
-				default:
-					exit(-1);
+			string typeName = (*params)[j]->getType()->getName();
+			if(typeName == "char")
+			{
+				__chan->ops->event_write(&__ctx, &j, sizeof(char));
+				lib_ring_buffer_align(__event_len, lttng_alignof(char));
+			}
+			else
+			{
+				__chan->ops->event_write(&__ctx, &j, sizeof(int));
+				lib_ring_buffer_align(__event_len, lttng_alignof(int));
 			}
 		}
 
